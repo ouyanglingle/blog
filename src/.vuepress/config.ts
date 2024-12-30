f25e74193ce771888c095bf59aadf83a538498de
graphwmz <ouyanglingle@outlook.com>
2024-12-31 00:38:18 +0800

    modified:   src/.vuepress/config.ts 	modified:   src/intro.md

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,8 +6,8 @@ export default defineUserConfig({
   base: "/blog",
 
   lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+  title: "博客",
+  description: "ouyanglingle 的博客",
 
   theme,
 
